dfcb2ffa37ab0dd8d54ac8a5dbc3d881776ae203
so2liu <so2liu@gamil.com>
2019-04-17 11:56:37 +0200

    new patching and unpatching subfuntion

--- utils/Patching.py
+++ utils/Patching.py
@@ -282,3 +282,126 @@ def get_random_indexs (image_shape, patch_size, index_list):
         index[2] = newIndex2 if (newIndex2 <= index2bound and newIndex2 >= 0) else index[2]
 
     return index_list
+
+
+def f3DPatching(img_origial, patch_size, overlap_rate):    
+    # so2liu@gmail.com, a small subfunction 'pad_size_calculate' in this file is used.  
+    # input: 3d np.array with zyx constellation, list with 3 elements in xyz constellation, a float
+    # output: 4d np.array with (n, x, y, z) constellation
+    
+    # use z y x constellation
+    patch_size.reverse()  # as z y x constellation
+    patch_size = np.array(patch_size, dtype=np.int32)
+    try:
+        assert len(img_origial.shape) == 3 and len(patch_size) == 3 and overlap_rate < 1
+    except Exception as e:
+        print('Only support 3D image as input. The input is', img_origial.shape, patch_size, overlap_rate)
+        print('Reason:', e)     
+    origin_size = img_origial.shape
+    overlap_pixel_yx = np.floor(overlap_rate*patch_size[1:]).astype(np.int32)
+    not_overlap_pixel_yx = np.ndarray.astype(patch_size[1:]-overlap_pixel_yx, np.int32)
+
+    # padding    
+    z_pad, y_pad, x_pad, _ = pad_size_calculate(origin_size, 
+                                             not_overlap_shape=(patch_size[0], not_overlap_pixel_yx[0], not_overlap_pixel_yx[1]), 
+                                             overlap_shape=(0, overlap_pixel_yx[0], overlap_pixel_yx[1]))
+    padded_img = np.pad(img_origial, (z_pad, y_pad, x_pad), 'constant')
+    assert not np.any(np.mod(padded_img.shape[1:], not_overlap_pixel_yx)-overlap_pixel_yx)  # ensure all zeros
+    assert not np.any(np.mod(padded_img.shape[0], patch_size[0]))  # ensure all zeros    
+        
+    # patching 
+    def fast_3D_strides(img, patch_shape, stepsize_tuple): 
+        z, y, x = img.shape
+        step_z, step_y, step_x = stepsize_tuple
+        sz, sy, sx = img.strides
+        
+        patch_frame_shape = np.divide(np.array(img.shape)-(np.array(patch_shape)-np.array(stepsize_tuple)), np.array(stepsize_tuple))
+        patch_frame_shape = tuple(patch_frame_shape.astype(int))  # big patch struction
+        result_shape = patch_frame_shape+tuple(patch_shape)
+        result_strides = (sx*step_z*x*y, sx*x*step_y, sx*step_x, sx*x*y, sx*x, sx)    
+        return np.lib.stride_tricks.as_strided(img, result_shape, result_strides)
+
+  
+    patched_img = fast_3D_strides(padded_img, patch_shape=patch_size, 
+                                  stepsize_tuple=(patch_size[0], 
+                                                  not_overlap_pixel_yx[0], 
+                                                  not_overlap_pixel_yx[1]))  # axis2 is full not-overlap
+    patched_img = np.reshape(patched_img, (-1, )+tuple(patch_size))
+
+    return np.swapaxes(patched_img, 1, -1)  # return (n, x, y, z) constellation
+
+
+def f3DUnpatching(patched_img, origin_shape, patch_size, overlap_rate):
+    # so2liu@gmail.com
+    # input: 4d imag with (n, x, y, z) as constellation, 1d oritin_shape (3 elements),  1d patch_size (3 elements), a float overlap_rate
+    # output: non-padded, non-seperated, non-overlapped 3d img with size of patch_size in zyx constellation
+    
+    assert len(patched_img.shape) == 4 and isinstance(patch_size, list)
+    
+    # change everything into z, y, x  constellation
+    patched_img = np.swapaxes(patched_img, 1, -1)  # switch x and z
+    patch_size.reverse()
+    patch_size = np.array(patch_size)
+
+    # calculate pad size
+    overlap_pixel_yx = np.floor(overlap_rate*patch_size[1:]).astype(np.int32)
+    not_overlap_pixel_yx = (patch_size[1:]-overlap_pixel_yx).astype(np.int32)
+    stepsize_tuple = (patch_size[0], not_overlap_pixel_yx[0], not_overlap_pixel_yx[1])
+    z_pad, y_pad, x_pad, padded_img_shape = pad_size_calculate(origin_shape, 
+                                             not_overlap_shape=(patch_size[0], not_overlap_pixel_yx[0], not_overlap_pixel_yx[1]), 
+                                             overlap_shape=(0, overlap_pixel_yx[0], overlap_pixel_yx[1]))    
+    rest_over_tuple = (0, overlap_pixel_yx[0], overlap_pixel_yx[1])
+    patch_frame_shape = tuple(np.divide(np.array(padded_img_shape)-np.array(rest_over_tuple), np.array(stepsize_tuple)).astype(int))  # big patch struction
+
+    non_overlap_patch_shape = (patch_size[0], not_overlap_pixel_yx[0], not_overlap_pixel_yx[1])
+    non_overlap_patched_img = np.zeros((patched_img.shape[0], )+non_overlap_patch_shape)
+    
+    # padded, seperated, overlapped img => padded, seperated, non-overlapped img
+    for n in range(patched_img.shape[0]):
+        non_overlap_patched_img[n, :, :, :] = patched_img[n, 0:patch_size[0], 0: not_overlap_pixel_yx[0], 0: not_overlap_pixel_yx[1]]
+    
+    framed_img = np.reshape(non_overlap_patched_img, patch_frame_shape+non_overlap_patch_shape)
+    
+    # padded, seperated, non-overlapped img => padded, non-seperated, non-overlapped img
+    result = np.ones(padded_img_shape)*255
+    overlap_framed_img = np.reshape(patched_img, patch_frame_shape+tuple(patch_size))
+    for fz in range(patch_frame_shape[0]):
+        for fy in range(patch_frame_shape[1]-1):  # -1 for ignore the right and bottom margin
+            for fx in range(patch_frame_shape[2]-1):
+                result[fz*non_overlap_patch_shape[0]:(fz+1)*non_overlap_patch_shape[0], 
+                       fy*non_overlap_patch_shape[1]:(fy+1)*non_overlap_patch_shape[1],
+                       fx*non_overlap_patch_shape[2]:(fx+1)*non_overlap_patch_shape[2]] = framed_img[fz, fy, fx, :, :, :]
+                # add the margin at right and bottom of the big img
+                if fy == patch_frame_shape[1]-2:
+                    result[fz*non_overlap_patch_shape[0]:(fz+1)*non_overlap_patch_shape[0], 
+                           (fy+1)*non_overlap_patch_shape[1]:,
+                           (fx)*non_overlap_patch_shape[2]:(fx+1)*non_overlap_patch_shape[2]] = overlap_framed_img[fz, fy+1, fx, :, :, :not_overlap_pixel_yx[1]]
+                    result[fz*non_overlap_patch_shape[0]:(fz+1)*non_overlap_patch_shape[0], 
+                           -patch_size[1]:,
+                           -patch_size[2]:] = overlap_framed_img[fz, fy+1, fx+1, :, :, :]
+                    
+                if fx == patch_frame_shape[2]-2:
+                    result[fz*non_overlap_patch_shape[0]:(fz+1)*non_overlap_patch_shape[0], 
+                           (fy)*non_overlap_patch_shape[1]:(fy+1)*non_overlap_patch_shape[1],
+                           (fx+1)*non_overlap_patch_shape[2]:] = overlap_framed_img[fz, fy, fx+1, :, :not_overlap_pixel_yx[0], :]
+    
+    # padded, non-seperated, non-overlapped img => non-padded, non-seperated, non-overlapped img
+    result = result[z_pad[0]:-z_pad[1], y_pad[0]:-y_pad[1], x_pad[0]:-x_pad[1]]
+    assert result.shape == tuple(origin_shape)    
+    return result/result.max() # normalization
+
+def pad_size_calculate(img_shape, not_overlap_shape, overlap_shape):
+    # so2liu@gmail.com, used by my f3DPatching and f3DUnpatching subfunctions
+
+    # a pad-calculation funciton, in z,y, x constellation
+    z, y, x = tuple(img_shape)
+    nonover_z, nonover_y, nonover_x = tuple(not_overlap_shape)
+    img_shape, not_overlap_shape, overlap_shape = np.array(img_shape), np.array(not_overlap_shape), np.array(overlap_shape)
+        
+    number_patches = np.ceil(np.divide(img_shape, not_overlap_shape)).astype(np.int32)
+    padded_img_shape = (np.multiply(number_patches, not_overlap_shape)+overlap_shape).astype(np.int32)
+    half_pad_shape = (padded_img_shape-img_shape).astype(np.float32)/2
+    x_pad = (np.ceil(half_pad_shape[2]).astype(np.int32), np.floor(half_pad_shape[2]).astype(np.int32))
+    y_pad = (np.ceil(half_pad_shape[1]).astype(np.int32), np.floor(half_pad_shape[1]).astype(np.int32))
+    z_pad = (0, (half_pad_shape[0]*2).astype(np.int32))
+    return z_pad, y_pad, x_pad, padded_img_shape
